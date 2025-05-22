31b57826f01b5141407f72cf64d0d48bf7ed5792
MRamsan <mohammedramsanp@gmail.com>
2025-05-22 20:14:49 +0530

    Add files via upload

---
+++ text_preprocessor.py
@@ -0,0 +1,17 @@
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+stop_words = set(stopwords.words('english'))
+punctuations = set(string.punctuation)
+wl = WordNetLemmatizer()
+
+def cleaning(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in stop_words and word not in punctuations]
+    tokens_lem = [wl.lemmatize(word) for word in tokens]
+    return ' '.join(tokens_lem)
